0505618e1d9f9f86c5940f194fcdfac358465533
usuario <jcmargentina@gmail.com>
2019-06-11 00:23:22 +0200

    migrations, models and basic routes

---
+++ step2_mini_blog/app/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = "posts";
+    protected $guarded = [];
+    protected $dates = [
+        "created_at",
+        "updated_at",
+    ];
+    
+    public function comments()
+    {
+        return $this->hasMany("App\Comment", "post_id", "id");
+    }
+}
