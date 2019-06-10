c1e8a382f9174e0d415188d4e3cc56e6f830922d
usuario <jcmargentina@gmail.com>
2019-06-11 00:22:10 +0200

    Cambio nombre de carpetas

--- mini_blog_mastertemplate_views/resources/views/layout/css.blade.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="{{ asset('css/all.css') }}">
-<link rel="stylesheet" href="{{ asset('css/bootstrap.min') }}.css" >
-<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
